4b070334807a8918e802b579afee0a76a7990acb
Tio Bagio <10209203+tiobagio@users.noreply.github.com>
2021-01-14 16:04:54 +0800

    update variable
    change some placeholder that specifies image

--- variables.tf
+++ variables.tf
@@ -44,6 +44,6 @@ variable "width" {
 }
 
 variable "placeholder" {
-  default     = "placekitten.com"
+  default     = "placedog.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
