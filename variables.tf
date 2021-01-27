8ea2b02700bede1cafcda589ad929dd135082efe
Tio Bagio <10209203+tiobagio@users.noreply.github.com>
2021-01-27 18:58:51 +0800

    change placeholder to dog

--- variables.tf
+++ variables.tf
@@ -44,6 +44,6 @@ variable "width" {
 }
 
 variable "placeholder" {
-  default     = "placekitten.com"
+  default     = "placedog.net"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
